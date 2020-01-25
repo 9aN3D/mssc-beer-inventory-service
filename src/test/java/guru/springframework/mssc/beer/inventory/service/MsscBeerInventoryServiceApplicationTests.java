268ed9944997140951d13658be8145453b0381b1
Volodymyr Kovalchuk <v.kovalchuk.n@gmail.com>
2020-01-25 21:05:36 +0100

    initial commit

---
+++ src/test/java/guru/springframework/mssc/beer/inventory/service/MsscBeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mssc.beer.inventory.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
