b53de5dea48ab0914ed0547d717ef6858cb057f8
Volodymyr Kovalchuk <v.kovalchuk.n@gmail.com>
2020-01-25 21:21:00 +0100

    Add domain layer

---
+++ src/main/java/guru/springframework/mssc/beer/inventory/service/domain/BeerInventory.java
@@ -0,0 +1,38 @@
+package guru.springframework.mssc.beer.inventory.service.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+public class BeerInventory extends BaseEntity {
+
+    private UUID beerId;
+
+    private String upc;
+
+    private Integer quantityOnHand = 0;
+
+    @Builder
+    public BeerInventory(UUID id,
+                         Long version,
+                         Timestamp createdDate,
+                         Timestamp lastModifiedDate,
+                         UUID beerId,
+                         String upc,
+                         Integer quantityOnHand) {
+        super(id, version, createdDate, lastModifiedDate);
+        this.beerId = beerId;
+        this.upc = upc;
+        this.quantityOnHand = quantityOnHand;
+    }
+
+}
